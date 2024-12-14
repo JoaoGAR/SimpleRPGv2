519867e1781ca7b681b9a205c23f171e84e20f4a
JoaoGAR <jgarchanjo@gmail.com>
2024-12-14 11:35:33 -0300

    Dungeon structure

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import CharacterCreation from './components/character/CharacterCreation';
 import Index from './components/world/Index';
 
 import City from './components/city/City';
+import Dungeon from './components/dungeon/Dungeon';
 import BattleCamp from './components/battle/battleCamp';
 
 import { AuthProvider } from './context/AuthContext';
@@ -23,18 +24,18 @@ function App() {
         <AuthProvider>
             <Router>
                 <Routes>
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/dashboard"
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/dashboard'
                         element={
                             <PrivateRoute>
                                 <Dashboard />
                             </PrivateRoute>
                         }
                     />
-                    <Route path="/" element={<Login />} />
+                    <Route path='/' element={<Login />} />
 
-                    <Route path="/character/register"
+                    <Route path='/character/register'
                         element={
                             <PrivateRoute>
                                 <CharacterCreation />
@@ -42,21 +43,28 @@ function App() {
                         }
                     />
 
-                    <Route path="/world"
+                    <Route path='/world'
                         element={
                             <PrivateRoute>
                                 <ItemInfoProvider><Index /></ItemInfoProvider>
                             </PrivateRoute>
                         }
                     />
-                    <Route path="/city/:cityName/:cityId"
+                    <Route path='/city/:cityName/:cityId'
                         element={
                             <PrivateRoute>
                                 <BattleRollsProvider><City /></BattleRollsProvider>
                             </PrivateRoute>
                         }
                     />
-                    <Route path="/battle/:targetId/:locationId"
+                    <Route path='/dungeon/:cityName/:cityId'
+                        element={
+                            <PrivateRoute>
+                                <BattleRollsProvider><Dungeon /></BattleRollsProvider>
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route path='/battle/:targetId/:locationId'
                         element={
                             <PrivateRoute>
                                 <BattleCamp />
